a40b632bac985353005e47f04d9cdb5950f0e1bd
Rajesh Majumdar <rajeshmjmdr02@gmail.com>
2022-05-29 13:26:28 +0530

    Finishing touches

---
+++ loss.py
@@ -0,0 +1,3 @@
+# Similar to activation functions, we have multiple loss functions too e.g. Mean Squared Error, should be familiar with this, since it is heavily used in linear regression. Another one is Categorical Cross-entropy, this is used in probabilities and mostly with softmax activation function.
+
+# And this is where i need to read more about probabilities. And here I thought probability is the easiest topic in mathematics
